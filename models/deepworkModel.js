32420eb60e8a919160250e4689e7c3e97b1a9d15
sanjay <sanjaykuttyags@gmail.com>
2024-12-15 14:16:27 +0530

    last

--- models/deepworkModel.js
+++ models/deepworkModel.js
@@ -5,12 +5,17 @@ const ActivitySchema = new mongoose.Schema({
   intensity: { type: String, enum: ['High', 'Medium', 'Low'], required: true },
 });
 
+const ActivitySchemaUnWind = new mongoose.Schema({
+  time: { type: String, required: true },
+  intensity: { type: String, enum: ['Yes' , 'No'], required: true },
+});
+
 const DeepWorkSchema = new mongoose.Schema({
   companyName: { type: String, required: true },
   userId: { type: String, required: true },
   Deepwork: [ActivitySchema],
   Distraction: [ActivitySchema],
-  Unwind: [ActivitySchema],
+  Unwind: [ActivitySchemaUnWind],
 }, { timestamps: true });
 
 export default mongoose.model('DeepWork', DeepWorkSchema);
